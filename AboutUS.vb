e2e8ca05fe6ba630fb596830c3c135eeb66a0578
junxian428 <holeyiho@gmail.com>
2022-06-17 10:07:28 +0800

    updates

--- AboutUS.vb
+++ AboutUS.vb
@@ -6,12 +6,20 @@
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-
+        Dim MyForm5 As New TicketBookingvb
+        MyForm5.Show()
+        Me.Hide()
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim MyForm4 As New BusSchedule
         MyForm4.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim MyForm6 As New ContactUs
+        MyForm6.Show()
         Me.Hide()
     End Sub
 End Class
---
+++ ContactUs.vb
@@ -0,0 +1,7 @@
+﻿Public Class ContactUs
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim MyForm4 As New AboutUS
+        MyForm4.Show()
+        Me.Hide()
+    End Sub
+End Class
